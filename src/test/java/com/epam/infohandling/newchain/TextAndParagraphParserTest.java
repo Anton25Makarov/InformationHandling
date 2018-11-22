9e156c474681a54fca53024b11e8f82165cb2c6b
Anton Makarov <nbsketmn250799@gmail.com>
2018-11-22 16:46:39 +0300

    made test to textParser

--- src/test/java/com/epam/infohandling/newchain/TextAndParagraphParserTest.java
+++ src/test/java/com/epam/infohandling/newchain/TextAndParagraphParserTest.java
@@ -4,16 +4,47 @@ import com.epam.infohandling.classes1.Component;
 import com.epam.infohandling.classes1.Composite;
 import com.epam.infohandling.classes1.Lexeme;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class TextAndParagraphParserTest {
-    private static final String TEXT = "First sentence. Second, sentence! 2 1 + sentence?";
+    private static final String PARAGRAPH = "First sentence. Second, sentence! 2 1 + sentence?\n";
+    private static final String TEXT = "    First sentence. 1 1 + sentence?\n" +
+            "    Third sentence. Fourth sentence 2 3 * 4 + ok?\n" +
+            "    Bye...";
 
-    private Component expectedComponent = new Composite();  // paragraph
+    private Component expectedParagraph = new Composite();  // paragraph
+    private Component expectedText = new Composite();  // whole text
 
-    @Before
-    public void init() {
+    @Test
+    public void shouldParseParagraph() {
+        // given
+        initParseParagraph();
+        ChainOfParsers sentenceParser = new SentenceParser();
+        ChainOfParsers paragraphParser = TextAndParagraphParser.paragraphParser(sentenceParser);
+
+        // when
+        Component actualComponent = paragraphParser.parse(PARAGRAPH);
+
+        // then
+        Assert.assertEquals(expectedParagraph, actualComponent);
+    }
+
+    @Test
+    public void shouldParseText() {
+        // given
+        initParseText();
+        ChainOfParsers sentenceParser = new SentenceParser();
+        ChainOfParsers paragraphParser = TextAndParagraphParser.paragraphParser(sentenceParser);
+        ChainOfParsers textParser = TextAndParagraphParser.textParser(paragraphParser);
+
+        // when
+        Component actualText = textParser.parse(TEXT);
+
+        // then
+        Assert.assertEquals(expectedText, actualText);
+    }
+
+    private void initParseParagraph() {
         Component firstComponent = Lexeme.word("First");
         Component secondComponent = Lexeme.word("sentence.");
 
@@ -36,21 +67,60 @@ public class TextAndParagraphParserTest {
         thirdSentence.addComponent(sixthComponent);
 
 
-        expectedComponent.addComponent(firstSentence);
-        expectedComponent.addComponent(secondSentence);
-        expectedComponent.addComponent(thirdSentence);
+        expectedParagraph.addComponent(firstSentence);
+        expectedParagraph.addComponent(secondSentence);
+        expectedParagraph.addComponent(thirdSentence);
     }
 
-    @Test
-    public void parse() {
-        // given
-        ChainOfParsers sentenceParser = new SentenceParser();
-        ChainOfParsers paragraphParser = TextAndParagraphParser.paragraphParser(sentenceParser);
+    private void initParseText() {
+        Component lexeme1 = Lexeme.word("First");
+        Component lexeme2 = Lexeme.word("sentence.");
+        Component lexeme3 = Lexeme.expression("1 1 +");
+        Component lexeme4 = Lexeme.word("sentence?");
+        Component lexeme5 = Lexeme.word("Third");
+        Component lexeme6 = Lexeme.word("sentence.");
+        Component lexeme7 = Lexeme.word("Fourth");
+        Component lexeme8 = Lexeme.word("sentence");
+        Component lexeme9 = Lexeme.expression("2 3 * 4 +");
+        Component lexeme10 = Lexeme.word("ok?");
+        Component lexeme11 = Lexeme.word("Bye...");
 
-        // when
-        Component actualComponent = paragraphParser.parse(TEXT);
+        Component sentence1 = new Composite();
+        Component sentence2 = new Composite();
+        Component sentence3 = new Composite();
+        Component sentence4 = new Composite();
+        Component sentence5 = new Composite();
 
-        // then
-        Assert.assertEquals(expectedComponent, actualComponent);
+        sentence1.addComponent(lexeme1);
+        sentence1.addComponent(lexeme2);
+
+        sentence2.addComponent(lexeme3);
+        sentence2.addComponent(lexeme4);
+
+        sentence3.addComponent(lexeme5);
+        sentence3.addComponent(lexeme6);
+
+        sentence4.addComponent(lexeme7);
+        sentence4.addComponent(lexeme8);
+        sentence4.addComponent(lexeme9);
+        sentence4.addComponent(lexeme10);
+
+        sentence5.addComponent(lexeme11);
+
+        Component paragraph1 = new Composite();
+        Component paragraph2 = new Composite();
+        Component paragraph3 = new Composite();
+
+        paragraph1.addComponent(sentence1);
+        paragraph1.addComponent(sentence2);
+
+        paragraph2.addComponent(sentence3);
+        paragraph2.addComponent(sentence4);
+
+        paragraph3.addComponent(sentence5);
+
+        expectedText.addComponent(paragraph1);
+        expectedText.addComponent(paragraph2);
+        expectedText.addComponent(paragraph3);
     }
 }
